dfd1bd3d2bd4eb353bfc03ddc90bfa013b6f9406
LukeFan <a5347354@gmail.com>
2019-12-02 21:51:38 +0800

    Add _ Unicode

--- cypress/cypress/integration/ML100.spec.js
+++ cypress/cypress/integration/ML100.spec.js
@@ -2,7 +2,7 @@
 /// <reference types="cypress-downloadfile"/>
 
 var text 
-const day = 5
+const day = Cypress.env("DAY")
 
 context('Actions', () => {
 
@@ -21,7 +21,7 @@ context('Actions', () => {
     cy.get('h2').should(($h2)=>{
       text = $h2.text()
       // text = text.replace(/[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/,"_");
-      text = text.replace(/[\uff1a_]/g,"_");
+      text = text.replace(/[\uff1a_|\u002F_]/g,"_");
       cy.downloadFile('https://pycrawler.cupoy.com/HomeworkAction.do?op=getHomeworkFileContent&hwid=D'+day+'&filepath=Day'+page+'_Sample.ipynb','../HomeWork/'+text,'Day'+page+'_Sample.ipynb');  
     })
   })
@@ -42,7 +42,7 @@ context('Actions', () => {
     cy.get('h2').should(($h2)=>{
       text = $h2.text()
       // text = text.replace(/[\u3002|\uff1f|\uff01|\uff0c|\u3001|\uff1b|\uff1a|\u201c|\u201d|\u2018|\u2019|\uff08|\uff09|\u300a|\u300b|\u3008|\u3009|\u3010|\u3011|\u300e|\u300f|\u300c|\u300d|\ufe43|\ufe44|\u3014|\u3015|\u2026|\u2014|\uff5e|\ufe4f|\uffe5]/,"_");
-      text = text.replace(/[\uff1a_]/g,"_");
+      text = text.replace(/[\uff1a_|\u002F_]/g,"_");
       cy.downloadFile('https://pycrawler.cupoy.com/HomeworkAction.do?op=getHomeworkFileContent&hwid=D'+day+'&filepath=Day'+page+'_HW.ipynb'    ,'../HomeWork/'+text,'Day'+page+'_HW.ipynb');
     })
   })
@@ -61,7 +61,7 @@ context('Actions', () => {
     var text
     cy.get('h2').should(($h2)=>{
       text = $h2.text()
-      text = text.replace(/[\uff1a_]/g,"_");
+      text = text.replace(/[\uff1a_|\u002F_]/g,"_");
     })
     cy.contains('下載')
       .invoke('attr', 'href')
